b3370d4d9c6bf09c6a098f617f96781c3e746d9c
jeevan-budha <jeevan.7budha@gmail.com>
2024-09-23 18:32:58 +0545

    LOgin Page

---
+++ script.js
@@ -0,0 +1,7 @@
+let button=document.querySelector("#btn");
+let userName=document.querySelector("#username");
+let password=document.querySelector("#password");
+
+button.addEventListener("click",(e)=>{
+    console.log("click is succes");
+})
